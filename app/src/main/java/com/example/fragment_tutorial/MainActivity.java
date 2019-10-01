09c6b6825932f3b92a43f2ed86695d221a36b45e
patrickfeltes <pj6444@gmail.com>
2019-10-01 14:48:27 -0500

    Create fragment project

---
+++ app/src/main/java/com/example/fragment_tutorial/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.fragment_tutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button fragment1Button = findViewById(R.id.fragment1Button);
+        Button fragment2Button = findViewById(R.id.fragment2Button);
+        final FrameLayout fragmentContainer = findViewById(R.id.fragment_container);
+
+        fragment1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = Fragment1.newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
+
+        fragment2Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment = Fragment2.newInstance();
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            }
+        });
+    }
+}
